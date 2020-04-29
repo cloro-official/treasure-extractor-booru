457b1a6a875a2a3fe1b2b25b60d23adce7da4dae
CLORO <60315633+cloro-official@users.noreply.github.com>
2020-04-29 18:31:45 +0800

    increased waittime

--- modules/directory.js
+++ modules/directory.js
@@ -36,10 +36,10 @@ exports.AttemptToDownloadImage = async function(url, path)
             console.log("Error trying to write file \"" + path + "\": " + error)
         })
 
-        request.setTimeout(15000, function()
+        request.setTimeout(30000, function()
         {
             request.abort()
-            console.log("Aborted since request exceeded 15 seconds.")
+            console.log("Aborted since request exceeded 30 seconds.")
 		})
     }
     catch(error)

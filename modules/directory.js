0e14dee732de53f2dd967fe913dea8d1d246c7f5
CLORO <60315633+cloro-official@users.noreply.github.com>
2020-05-09 04:31:24 +0800

    Added "All Site" feature, optimized naming schemes

--- modules/directory.js
+++ modules/directory.js
@@ -1,5 +1,12 @@
 const http = require("https")
 const fs = require("fs-extra")
+const progress = require("cli-progress")
+const xhr = require("xmlhttprequest").XMLHttpRequest
+const XMLHttpRequest = new xhr()
+
+const wait = (s) => {
+    return new Promise(resolve => setTimeout(resolve, s * 1000))
+}
 
 exports.CreateDirectory = async function(path, options = {mode: 0o2775})
 {
@@ -13,22 +20,70 @@ exports.CreateDirectory = async function(path, options = {mode: 0o2775})
     }
 }
 
+exports.GetFileSize = async function(url)
+{
+    var fileSize = ''
+    XMLHttpRequest.open('HEAD', url, false)
+
+    XMLHttpRequest.send(null)
+
+    if (XMLHttpRequest.status === 200) {
+        fileSize = XMLHttpRequest.getResponseHeader('content-length');
+    }
+
+    return fileSize;
+}
+
 exports.AttemptToDownloadImage = async function(url, path, limit)
 {
     if (!url) {return}
     let result = 0
     let file = fs.createWriteStream(path)  
     
-    console.log("Attempting to get URL: " + url)
+    console.log("Attempting to get URL: " + url)/*
+    const bar = new progress.SingleBar({
+        stopOnComplete: true,
+        format: "Writing... [{bar}] | {percentage} | ETA: {eta}s | {value}/{total}"
+    }, progress.Presets.shades_classic)*/
+
     try
     {
+        /*
+        var fileSize = this.GetFileSize(url)
+        bar.start(fileSize || 0, 0, {
+            speed: "Undefined"
+        })*/
+
         let request = http.get(url, function(response)
         {
             response.pipe(file)
+            let isDone = false
+
             file.on("finish", function()
             {
-                console.log("Wrote file at " + path + " with size: " + file.bytesWritten/1048576 + " megabytes")
+                isDone = true
+                let bytes = file.bytesWritten/1048576
+                
+                console.log("Wrote file at " + path + " with size: " + Math.round(bytes * 100) / 100 + " megabytes")
+                //bar.stop()
 		    })
+
+            file.on("open", function()
+            {
+                console.log("Starting request for " + path)
+            })
+            /*
+            file.on("ready", function()
+            {
+                let LastWritten = file.bytesWritten
+                while (isDone == false) 
+                {
+                    bar.update(file.bytesWritten - LastWritten)
+
+                    LastWritten = file.bytesWritten
+                    wait(1/30)
+                }   
+            })*/
 	    }).on("error", function(error)
         {
             fs.unlink(path)
